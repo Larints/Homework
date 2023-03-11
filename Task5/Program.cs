6ced8d2fd4899d5a1b7f97cf0deace959dbc762d
Larin <lagoss@yandex.ru>
2023-03-11 18:39:15 +0300

    add new nomework from seminar 2

---
+++ Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Строковый метод
+
+// Console.WriteLine("Введите трёхзначное число: ");
+// string number = Console.ReadLine()!;
+// if (number.Length  == 3){
+//     Console.WriteLine($"Вторая цифра введенного числа {number[1]}");
+// }
+// else{
+//     Console.WriteLine("Введенно неверное число");
+// }
+
+// Математический метод
+
+Console.WriteLine("Генерируем случайное трёхзначное число: ");
+int num = new Random().Next(100, 1000);
+Console.WriteLine($" Выводим это число -> {num}");
+int result = num % 100;
+int finish = result / 10;
+Console.WriteLine($"Вторая цифра введенного числа -> {finish}");
---
+++ Task7/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите день недели: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number > 7)
+{
+    Console.WriteLine("Увы такого дня недели нет");
+}
+if (number < 6){
+    Console.WriteLine("Сегодня будничный день");
+}
+else
+{
+    if (number == 6)
+    {
+        Console.WriteLine("Сегодня суббота");
+    }
+    if (number == 7)
+    {
+        Console.WriteLine("Сегодня воскресенье");
+    }
+}
