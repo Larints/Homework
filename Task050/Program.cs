b98af71ab5870179cc2164a1b7e48004327226a7
Larin <lagoss@yandex.ru>
2023-03-30 23:44:43 +0300

    add new homework from seminar 7

---
+++ Task050/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает два числа
+// (строка, столбец), проверяя есть ли такая позиция в двумерном массиве 
+// и возвращает сообщение о том, что оно найдено, 
+// а также какое число стоит на этом месте или же указание, 
+// что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// i = 1, j = 3 -> Такой элемент есть: 3
+// i = 4, j = 2 -> такого элемента в массиве нет
+
+
+Console.WriteLine("Введите номер искомой строки: ");
+
+int row = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите номер искомого столбца: ");
+
+int column = int.Parse(Console.ReadLine()!);
+
+int[,] SomeArray = new int[5, 5];
+
+FillArray(SomeArray);
+PrintArray(SomeArray);
+SearhcingElement(SomeArray);
+
+
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SearhcingElement(int[,] array)
+{
+    if (row < array.GetLength(0) && column < array.GetLength(1))
+    {
+        Console.WriteLine($"Такой элемент есть: {array[row, column]}");
+    }
+    else
+    {
+        Console.WriteLine("Такого элемента в массиве нет");
+    }
+}
+
+
+
---
+++ Task052/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+int[,] SomeArray = new int[4, 4];
+
+FillArray(SomeArray);
+PrintArray(SomeArray);
+GetAverage(SomeArray);
+
+
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetAverage(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+
+    for (int j = 0; j < cols; j++)
+    {
+        double sum = 0;
+
+        for (int i = 0; i < rows; i++)
+        {
+            sum += array[i, j];
+        }
+
+        double average = sum / rows;
+        Console.WriteLine($"Среднее арифметическое столбца {j}: {average}");
+    }
+}
