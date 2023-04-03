85cbbb0804e4485d3dc7be358918d43184b240a7
Larin <lagoss@yandex.ru>
2023-04-03 23:34:46 +0300

    reworking

--- Task060/Program.cs
+++ Task060/Program.cs
@@ -20,16 +20,11 @@ PrintArray(SomeArray);
 
 int[] Randomizer (int[] array)
 {
-     for (int i =0; i < array.Length; i++)
+    int count = 10; 
+    for (int i =0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(1,100);
-    }
-    for (int i =0; i < array.Length-1; i++)
-    {
-        if (array[i] == array[i+1])
-        {
-            array[i+1] = array[i] + 1;
-        }
+        array[i] = count;
+        count ++;
     }
     return array;
 }
